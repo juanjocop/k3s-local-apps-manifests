4a29e24472ab04a8569713f737dbd87570d952de
Juan José Copete Jiménez <juanjocop@gmail.com>
2024-10-25 17:49:30 +0200

    cambios, desplegamos cosas con helm

--- cronjobs/script/update-cloudflare-dns.sh
+++ cronjobs/script/update-cloudflare-dns.sh
@@ -5,6 +5,7 @@
 # - CF_ZONE_ID: El Zone ID de tu dominio en Cloudflare
 # - CF_RECORD_NAME: El nombre del registro DNS que quieres actualizar (por ejemplo, tu-dominio.com)
 # - CF_RECORD_TYPE: El tipo de registro DNS (probablemente A para IPv4 o AAAA para IPv6)
+# - CF_PROXIED: Si queremos que este protegida bajo proxy o que apunte directamente a nuestra ip
 
 # Obtener la dirección IP externa actual
 IP=$(curl -s http://ipinfo.io/ip)
@@ -18,6 +19,6 @@ RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_
 RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$RECORD_ID" \
      -H "Authorization: Bearer $CF_API_KEY" \
      -H "Content-Type: application/json" \
-     --data '{"type":"'"$CF_RECORD_TYPE"'","name":"'"$CF_RECORD_NAME"'","content":"'"$IP"'","proxied":true}')
+     --data '{"type":"'"$CF_RECORD_TYPE"'","name":"'"$CF_RECORD_NAME"'","content":"'"$IP"'","proxied":"'"$CF_PROXIED"'"}')
 
 echo $RESPONSE
